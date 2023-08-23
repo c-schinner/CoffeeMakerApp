9cdbda891d0fbc4f80876f0727a39c8c10574d95
Chris Schinner <cschinner7@gmail.com>
2023-08-22 20:44:46 -0700

    coffee maker app

---
+++ main.py
@@ -0,0 +1,101 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+profit = 0
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+
+is_on = True
+
+
+def count_change():
+    print("Please insert some coins.")
+    change = int(input("How many pennies? : ")) * .01
+    change += int(input("How many nickels? : ")) * .05
+    change += int(input("How many dimes? : ")) * .10
+    change += int(input("How many quarters? : ")) * .25
+    return change
+
+
+def resources_check(order_ingredients):
+    for item in order_ingredients:
+        if order_ingredients[item] >= resources[item]:
+            print(f"Sorry there is not enough resources to make {item}.")
+            return False
+    return True
+
+
+def transaction(money_received, drink_cost):
+    if money_received >= drink_cost:
+        change = round(money_received - drink_cost, 2)
+        print(f"Here is ${change} in change.")
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("Sorry that is not enough money, MONEY REFUNDED.")
+        return False
+
+
+def make_coffee(drink_name, order_ingredients):
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name}, Enjoy")
+
+
+print('''
+--------------------
+        MENU
+--------------------
+[Espresso] [$ 1.50]
+[Latte] [$ 2.50]
+[Cappuccino] [$ 3.00]
+--------------------
+''')
+
+
+while is_on:
+    print("Type 'report' to check the machines resources.")
+    choice = input("What would you like? (Espresso/Latte/Cappuccino) : ").lower()
+
+    if choice == 'report':
+        print(f"Water : {resources['water']}ml")
+        print(f"Milk : {resources['milk']}ml")
+        print(f"Coffee : {resources['coffee']}g")
+        print(f"Money : ${profit}")
+
+    elif choice == "off":
+        is_on = False
+
+    else:
+        drink = MENU[choice]
+        if resources_check(drink['ingredients']):
+            payment = count_change()
+            if transaction(payment, drink['cost']):
+                make_coffee(choice, drink['ingredients'])
